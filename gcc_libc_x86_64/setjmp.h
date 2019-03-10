3d07b3f45b98ff1969a0e6ee9d259488c1efc618
Michael Gran <spk121@yahoo.com>
2019-03-09 18:16:40 -0800

    fixes for math.h, setjmp.h

--- gcc_libc_x86_64/setjmp.h
+++ gcc_libc_x86_64/setjmp.h
@@ -1,18 +1,26 @@
 #ifndef _SETJMP_H
 #define _SETJMP_H
 
-typedef struct {unsigned long int __val[16];} __sigset_t;
+typedef struct
+{
+    unsigned long int __val[16];
+} __sigset_t;
+
 typedef long int __jmp_buf[8];
 
 struct __jmp_buf_tag
-  {
-    __jmp_buf __jmpbuf;		/* Calling environment.  */
-    int __mask_was_saved;	/* Saved the signal mask?  */
-    __sigset_t __saved_mask;	/* Saved signal mask.  */
-  };
-typedef struct __jmp_buf_tag jmp_buf[1];
-int _setjmp (jmp_buf env);
-#define setjmp(env) _setjmp(env)
-_Noreturn void longjmp(jmp_buf env, int val);
+{
+    __jmp_buf __jmpbuf;
+    int __mask_was_saved;
+    __sigset_t __saved_mask;
+};
 
+typedef struct __jmp_buf_tag jmp_buf[1];
+
+extern int setjmp (jmp_buf env)
+    __attribute__ ((__nothrow__));
+extern void longjmp(struct __jmp_buf_tag __env[1],
+                    int __val)
+    __attribute__ ((__nothrow__))
+    __attribute__ ((__noreturn__));
 #endif
