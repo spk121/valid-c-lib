489c40b839b608b38de7404f9071472d797d6dec
Michael Gran <spk121@yahoo.com>
2019-03-13 02:23:13 -0700

    extra parens in stdarg.h harmful

--- headers/gcc_libc_x86_64/stdarg.h
+++ headers/gcc_libc_x86_64/stdarg.h
@@ -3,9 +3,9 @@
 
 typedef __builtin_va_list va_list;
 
-#define va_arg(ap, type) __builtin_va_arg((ap), (type))
+#define va_arg(v,l) __builtin_va_arg(v,l)
 #define va_copy(dest, src) __builtin_va_copy((dest), (src))
-#define va_end(ap) __builtin_va_end(ap)
-#define va_start(ap, parmN), __builtin_va_start((ap),(parmN))
+#define va_end(v) __builtin_va_end(v)
+#define va_start(v,l) __builtin_va_start(v,l)
 
 #endif
